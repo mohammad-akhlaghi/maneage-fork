e8ed6d942bede306fd455d02743d5d991fe2b01a
Mohammad Akhlaghi <mohammad@akhlaghi.org>
2025-06-10 21:41:27 +0200

    Configuration: temporary corrections removed
    Until this commit, some remaining temporary modifications (in
    'reproduce/software/make/high-level.mk') had remained! These were made
    temporarily to allow a fast build on my local system, but I forgot to
    remove them before pushing.
    
    With this commit, they have been revered to the same version on the Maneage
    branch (since they are unrelated to the A&A journal modifications).

--- reproduce/software/make/high-level.mk
+++ reproduce/software/make/high-level.mk
@@ -444,7 +444,6 @@ $(ibidir)/cfitsio-$(cfitsio-version):
 #
 #	Note that older versions of CFITSIO (before 4.4.0) require a
 #	specific 'shared' target for the building of the shared libraries.
-	export CFLAGS="-std=gnu17 $$CFLAGS"
 	export gbuild_tar=$(ddir)/$$customtar
 	$(call gbuild, cfitsio-$(cfitsio-version), , \
 	               --enable-sse2 --enable-reentrant \
@@ -1199,8 +1198,8 @@ $(ibidir)/ghostscript-$(ghostscript-version): \
 	            --enable-dynamic \
 	            --disable-compile-inits \
 		    --disable-hidden-visibility \
-		    LDFLAGS=-Wl,--copy-dt-needed-entries \
-		    CFLAGS="-DPNG_ARM_NEON_OPT=0 -std=gnu17"
+		    CFLAGS="-DPNG_ARM_NEON_OPT=0" \
+		    LDFLAGS=-Wl,--copy-dt-needed-entries
 
 #	Build and install the program and the shared libraries.
 	make    V=1 -j$(numthreads)
@@ -1230,6 +1229,7 @@ $(ibidir)/gnuastro-$(gnuastro-version): \
                    $(ibidir)/wcslib-$(wcslib-version) \
                    $(ibidir)/libjpeg-$(libjpeg-version) \
                    $(ibidir)/libtiff-$(libtiff-version) \
+                   $(ibidir)/libgit2-$(libgit2-version) \
                    $(ibidir)/ghostscript-$(ghostscript-version)
 	tarball=gnuastro-$(gnuastro-version).tar.lz
 	$(call import-source, $(gnuastro-url), $(gnuastro-checksum))
@@ -1865,6 +1865,7 @@ $(ibidir)/zip-$(zip-version): $(ibidir)/gzip-$(gzip-version)
 # NY on the internet infrastructure).
 texlive-url=http://mirrors.rit.edu/CTAN/systems/texlive/tlnet
 $(itidir)/texlive-ready-tlmgr: reproduce/software/config/texlive.conf
+
 	tarball=install-tl-unx.tar.gz
 	$(call import-source, $(texlive-url), NO-CHECK-SUM)
 
